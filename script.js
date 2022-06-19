f7592ec6c867fcd8ca5067234e5c28b32533906b
SaiPrakashGit <prakashrayapati146@gmail.com>
2022-06-19 18:44:38 +0530

    Reinitialize the repo

--- script.js
+++ script.js
@@ -1,78 +1,49 @@
-const prevBtn = document.getElementById('prev') ;
-const playBtn = document.getElementById('play') ;
-const nextBtn = document.getElementById('next') ;
-const audio = document.getElementById('audio') ;
-const title = document.getElementById('title') ;
-const progress = document.getElementById('progress') ;
-const progressContainer = document.getElementById('progresscontainer') ;
-const musicContainer = document.getElementById('musiccontainer') ;
+// JavaScript logic to switch themes and store it in  browser's storage
 
-const songs = ['1. Ay Pilla', '2. Evo Evo Kalale', '3. Nee Chitram Choosi', '4. Sarangadariya', '5. NEFFEX - Flirt (Bonus)'] ;
+let theme = localStorage.getItem("theme");
 
-let songindex = 0 ;
-
-// Load Song
-function loadSong(song) {
-    title.innerText = song ;
-    audio.src = `music/${song}.mp3` ;
+if (theme == null) {
+  setTheme("blue");
+} else {
+  setTheme(theme);
 }
 
-// Initial Song load
-loadSong(songs[songindex]) ;
+let themeDots = document.getElementsByClassName("theme-dot");
 
-// Play Song
-function playSong() {
-    musicContainer.classList.add('play') ;
-    playBtn.querySelector('i.fas').classList.remove('fa-play');
-    playBtn.querySelector('i.fas').classList.add('fa-pause');
-    audio.play();
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    setTheme(mode);
+    playTone();
+  });
 }
 
-// Pause Song
-function pauseSong() {
-    musicContainer.classList.remove('play') ;
-    playBtn.querySelector('i.fas').classList.add('fa-play');
-    playBtn.querySelector('i.fas').classList.remove('fa-pause');
-    audio.pause();
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "light.css";
+  }
+
+  if (mode == "blue") {
+    document.getElementById("theme-style").href = "blue.css";
+  }
+
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "green.css";
+  }
+
+  if (mode == "purple") {
+    document.getElementById("theme-style").href = "purple.css";
+  }
+
+  localStorage.setItem("theme", mode);
 }
 
-// prevSong
-function prevSong() {
-    songindex-- ;
-    if(songindex<0){ 
-        songindex = songs.length - 1 ;
-        // alert('This is the beginning of Songs Queue.. Do you want to go to the end of Queue instead ?') ;
-    }
-    loadSong(songs[songindex]) ;
-    playSong() ;
+const tone = document.getElementById("audio");
+tone.volume = 0.5;
+
+tone.src = `assets/click_tone.mp3`;
+
+// Play Click-Tone
+function playTone() {
+  tone.play();
 }
-
-// nextSong
-function nextSong() {
-    songindex++ ;
-    if(songindex>songs.length - 1) { 
-        songindex = 0 ;
-        // alert('End of the Queue Reached.. Shall we loop the songs again ?') ;
-    }
-    loadSong(songs[songindex]) ;
-    playSong() ;
-}
-// Event Listeners from User clicks
-playBtn.addEventListener('click', () => {
-    const playstate = musicContainer.classList.contains('play') ;
-
-    if (playstate) { pauseSong() ; }
-    else { playSong() ; }
-});
-
-prevBtn.addEventListener('click', prevSong) ;
-nextBtn.addEventListener('click', nextSong) ;
-// Call nextSong after a song ends
-audio.addEventListener('ended', nextSong) ;
-// Update progress
-audio.addEventListener('timeupdate', updateProgress) ;
-// Update Time
-audio.addEventListener('timeupdate', updateTime) ;
-// Update Progress bar
-progressContainer.addEventListener('click', setProgress) ;
-// 
